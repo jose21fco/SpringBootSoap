9abe61c2b36cb61ae6b4d4b437d9f8af27b71777
Jose Echavarria <jose.echavarria@mzpsolutions.com>
2023-07-09 13:38:29 -0500

    app spring boot

---
+++ src/main/java/com/soap/client/SoapClient.java
@@ -0,0 +1,22 @@
+package com.soap.client;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+import com.soap.wsdl.AddResponse;
+import com.soap.wsdl.Add;
+
+public class SoapClient extends WebServiceGatewaySupport {
+    
+    public AddResponse getAddResponse(int numberA, int numberB){
+        Add addRequest = new Add();
+        addRequest.setIntA(numberA);
+        addRequest.setIntB(numberB);
+
+        SoapActionCallback soapActionCallback = new SoapActionCallback("http://tempuri.org/Add");
+
+        AddResponse addResponse = (AddResponse) getWebServiceTemplate().marshalSendAndReceive("http://www.dneonline.com/calculator.asmx", addRequest, soapActionCallback);
+
+        return addResponse;
+    }
+}
---
+++ src/main/java/com/soap/config/SoapConfig.java
@@ -0,0 +1,28 @@
+package com.soap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+import com.soap.client.SoapClient;
+
+@Configuration
+public class SoapConfig {
+
+    @Bean
+    public Jaxb2Marshaller marshaller(){
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("com.soap.wsdl");
+        return marshaller;
+    }
+
+    @Bean
+    public SoapClient getSoapClient(Jaxb2Marshaller marshaller){
+        SoapClient soapClient = new SoapClient();
+        soapClient.setDefaultUri("http://www.dneonline.com/calculator.asmx");
+        soapClient.setMarshaller(marshaller);
+        soapClient.setUnmarshaller(marshaller);
+
+        return soapClient;
+    }
+}
